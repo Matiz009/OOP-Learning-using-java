421e226949b559cfba163273160e9a91624fe057
MATI UL REHMAN <iammatiz00@gmail.com>
2023-06-28 14:50:07 +0500

    finalization added;

--- introduction/lecture_1/src/Main.java
+++ introduction/lecture_1/src/Main.java
@@ -10,6 +10,10 @@ public class Main {
         System.out.println(ahmad.name+": "+ahmad.marks);
         mati.marks=450;
         System.out.println(mati.name+": "+mati.marks);
+        Student std;
+        for (int i = 0; i < 10000000; i++) {
+            std=new Student(400,"Random Name","SP20-BSE-00");
+        }
 
     }
 }
--- introduction/lecture_1/src/Student.java
+++ introduction/lecture_1/src/Student.java
@@ -3,9 +3,14 @@ public class Student {
     String name;
     String regNumber;
     Student(int marksIn,String nameIn, String regNumberIn){
+        System.out.println("Object is created..");
         this.name=nameIn;
         this.regNumber=regNumberIn;
         this.marks=marksIn;
     }
 
+    @Override//METHOD USE TO DESTROY THE OBJECTS AUTOMATICALLY BY JAVA
+    protected void finalize() throws Throwable {
+        System.out.println("Object is destroyed");
+    }
 }
